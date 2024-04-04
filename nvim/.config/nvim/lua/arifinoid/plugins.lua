419f9bd976d150c5e96d142d5343ed85dd821074
arifinoid <rohmadarifin007@gmail.com>
2024-04-04 15:03:01 +0700

    feat: add silicon for code snapshot

--- nvim/.config/nvim/lua/arifinoid/plugins.lua
+++ nvim/.config/nvim/lua/arifinoid/plugins.lua
@@ -224,4 +224,23 @@ return {
 
 	-- platform
 	"wakatime/vim-wakatime",
+	{
+		"michaelrommel/nvim-silicon",
+		lazy = true,
+		cmd = "Silicon",
+		init = function()
+			local wk = require("which-key")
+			wk.register({
+				["<leader>sc"] = { ":Silicon<CR>", "[S]ilicon snapshot code" },
+				{ mode = "v" },
+			})
+		end,
+		config = function()
+			require("silicon").setup({
+				font = "JetBrainsMono Nerd Font=34;Noto Color Emoji=34",
+				theme = "Dracula",
+				background = "#94e2d5",
+			})
+		end,
+	},
 }
